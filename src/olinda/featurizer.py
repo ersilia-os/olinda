1e78cfe23eaa8672c82478875dbb6a90852d2c6c
Ankur Kumar <ank@leoank.me>
2022-09-14 16:27:07 +0530

    update featurizer class and related api
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/olinda/featurizer.py
+++ src/olinda/featurizer.py
@@ -1,17 +1,67 @@
 """Featurizer for SMILES."""
 
+from abc import ABC
+from typing import Any, List
+
 import joblib
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
 
 from olinda.utils import get_package_root_path
 
 
-class Featurizer:
-    def __init__(self: "Featurizer", mode: str) -> None:
-        """Init
+class Featurizer(ABC):
+    def featurize(self: "Featurizer", batch: Any) -> Any:
+        """Featurize input batch.
 
         Args:
-            mode (str): Type of featurizer to use.
+            batch (Any): batch of smiles
+
+        Returns:
+            Any: featurized outputs
         """
-        if mode == "flat2grid":
-            self.transformer = joblib.load(get_package_root_path() / "flat2grid.joblib")
-            self.name = "flat2grid"
+        pass
+
+
+class Flat2Grid(Featurizer):
+    def __init__(self: "Flat2Grid") -> None:
+        self.transformer = joblib.load(get_package_root_path() / "flat2grid.joblib")
+        self.name = "flat2grid"
+
+    def featurize(self: "Flat2Grid", batch: Any) -> Any:
+        """Featurize input batch.
+
+        Args:
+            batch (Any): batch of smiles
+
+        Returns:
+            Any: featurized outputs
+        """
+
+        mols = [Chem.MolFromSmiles(smi) for smi in batch]
+        ecfps = self.ecfp_counts(mols)
+        return self.transformer.transform(ecfps)
+
+    def ecfp_counts(self: "Flat2Grid", mols: List) -> List:
+        """Create ECFPs from batch of smiles.
+
+        Args:
+            mols (List): batch of molecules
+
+        Returns:
+            List: batch of ECFPs
+        """
+        fps = [
+            AllChem.GetMorganFingerprint(
+                mol, radius=3, useCounts=True, useFeatures=True
+            )
+            for mol in mols
+        ]
+        nfp = np.zeros((len(fps), 1024), np.uint8)
+        for i, fp in enumerate(fps):
+            for idx, v in fp.GetNonzeroElements().items():
+                nidx = idx % 1024
+                nfp[i, nidx] += int(v)
+        return nfp
