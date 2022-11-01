5fb284ee8ff1a37b91ea2e6c269d8fbd96541c1e
Ankur Kumar <ank@leoank.me>
2022-11-02 03:03:08 +0530

    fix tensor dataset wrapper
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/olinda/data/tensor_dataset_wrapper.py
+++ src/olinda/data/tensor_dataset_wrapper.py
@@ -14,36 +14,39 @@ class TensorflowDatasetWrapper:
         datamodule: GenericOutputDM,
         stage: str = "train",
         only_X: bool = True,
-        only_Y: bool = False,
+        only_Y: bool = True,
     ):
-        if only_X:
-            datamodule.setup(stage, only_X=True, batched=False)
-            self.dataset = datamodule.dataset
-            sample = next(iter(self.dataset))
-            sample = np.array(sample)
-            self.output_shape = (32, 32)
-        elif only_Y:
-            datamodule.setup(stage, only_Y=True, batched=False)
-            self.dataset = datamodule.dataset
-            sample = next(iter(self.dataset))
-            sample = np.array(sample)
-            self.output_shape = sample.shape
-        else:
-            raise Exception("Select either only X or only Y")
+        self.only_X = only_X
+        self.only_Y = only_Y
+
+        datamodule.setup(stage, only_X, only_Y, batched=False)
+        self.dataset = datamodule.dataset
+        sample = next(iter(self.dataset))
+        sample = np.array(sample)
+
         if stage == "train":
             self.loader = datamodule.train_dataloader()
         elif stage == "val":
             self.loader = datamodule.val_dataloader()
 
+        if only_X and only_Y:
+            self.output_sign = (
+                tf.TensorSpec(shape=np.array(sample[0]).shape, dtype=tf.float32),
+                tf.TensorSpec(shape=np.array(sample[1]).shape, dtype=tf.float32),
+            )
+        else:
+            self.output_sign = (tf.TensorSpec(shape=sample.shape, dtype=tf.float32),)
+
     def __iter__(self):
         for sample in self.loader:
-            yield np.array(sample).astype("float32")
+            if self.only_X and self.only_Y:
+                x, y = sample
+                yield (np.array(x), np.array(y))
+            else:
+                yield np.array(sample)
 
     def __len__(self):
         return self.length
 
-    def output_shapes(self):
-        return (self.output_shape, (len(self.loader.length),))
-
-    def output_types(self):
-        return (tf.float32,)
+    def output_signature(self):
+        return self.output_sign
